4fd702f5179470e9acbd1f1baf84358939018484
osk <osk@osk.com>
2022-05-12 09:27:37 +0800

    SMM: remove currently useless code

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -9071,61 +9071,6 @@ struct page *smm_alloc_zeroed_user_highpage_movable(struct vm_fault *vmf)
 	return page;
 }
 
-static int pmd_devmap_trans_unstable(pmd_t *pmd)
-{
-	return pmd_devmap(*pmd) || pmd_trans_unstable(pmd);
-}
-
-static vm_fault_t pte_alloc_one_map(struct vm_fault *vmf)
-{
-	struct vm_area_struct *vma = vmf->vma;
-
-	if (!pmd_none(*vmf->pmd))
-		goto map_pte;
-	if (vmf->prealloc_pte) {
-		vmf->ptl = pmd_lock(vma->vm_mm, vmf->pmd);
-		if (unlikely(!pmd_none(*vmf->pmd))) {
-			spin_unlock(vmf->ptl);
-			goto map_pte;
-		}
-
-		mm_inc_nr_ptes(vma->vm_mm);
-		pmd_populate(vma->vm_mm, vmf->pmd, vmf->prealloc_pte);
-		spin_unlock(vmf->ptl);
-		vmf->prealloc_pte = NULL;
-	} else if (unlikely(pte_alloc(vma->vm_mm, vmf->pmd))) {
-		return VM_FAULT_OOM;
-	}
-map_pte:
-	/*
-	 * If a huge pmd materialized under us just retry later.  Use
-	 * pmd_trans_unstable() via pmd_devmap_trans_unstable() instead of
-	 * pmd_trans_huge() to ensure the pmd didn't become pmd_trans_huge
-	 * under us and then back to pmd_none, as a result of MADV_DONTNEED
-	 * running immediately after a huge pmd fault in a different thread of
-	 * this mm, in turn leading to a misleading pmd_trans_huge() retval.
-	 * All we have to ensure is that it is a regular pmd that we can walk
-	 * with pte_offset_map() and we can do that through an atomic read in
-	 * C, which is what pmd_trans_unstable() provides.
-	 */
-	if (pmd_devmap_trans_unstable(vmf->pmd))
-		return VM_FAULT_NOPAGE;
-
-	/*
-	 * At this point we know that our vmf->pmd points to a page of ptes
-	 * and it cannot become pmd_none(), pmd_devmap() or pmd_trans_huge()
-	 * for the duration of the fault.  If a racing MADV_DONTNEED runs and
-	 * we zap the ptes pointed to by our vmf->pmd, the vmf->ptl will still
-	 * be valid and we will re-check to make sure the vmf->pte isn't
-	 * pte_none() under vmf->ptl protection when we return to
-	 * alloc_set_pte().
-	 */
-	vmf->pte = pte_offset_map_lock(vma->vm_mm, vmf->pmd, vmf->address,
-			&vmf->ptl);
-	return 0;
-}
-
-
 
 void smm_do_read_fault(struct vm_fault *vmf)
 {
@@ -9138,8 +9083,6 @@ void smm_do_read_fault(struct vm_fault *vmf)
 	struct zone *zone;
 	struct vm_area_struct *vma = vmf->vma;
 	unsigned long address = vmf->address;
-	pte_t entry;
-	bool write = vmf->flags & FAULT_FLAG_WRITE;
 
 	if (!vmf->vma->vm_mm->smm_activate)
 		return;
@@ -9161,25 +9104,6 @@ void smm_do_read_fault(struct vm_fault *vmf)
 	zone = page_zone(page);
 	alloc_flags = gfp_to_alloc_flags(GFP_HIGHUSER_MOVABLE);
 
-#if 0
-	entry = mk_pte(page, vma->vm_page_prot);
-	entry = pte_sw_mkyoung(entry);
-	if (write)
-		entry = maybe_mkwrite(pte_mkdirty(entry), vma);
-
-	if (!vmf->pte) {
-		ret = pte_alloc_one_map(vmf);
-		if (ret)
-			return;
-	}
-	/* For now we hold ptl */
-
-	if (!pte_none(*vmf->pte) && pte_pfn(*vmf->pte) == pfn) {
-		get_page(page); // For later VM_FAULT_NOPAGE
-		pte_unmap_unlock(vmf->pte, vmf->ptl);
-		return;
-	}
-#endif
 	ret = take_smm_page_off_buddy(page);
 
 	if (ret) {
@@ -9211,18 +9135,7 @@ void smm_do_read_fault(struct vm_fault *vmf)
 		ret = alloc_contig_range(pfn, pfn+1, MIGRATE_CMA, GFP_HIGHUSER_MOVABLE);
 		smm_unlock();
 		if (ret == 0) {
-#if 0
-			/* copy-on-write page */
-			if (write && !(vma->vm_flags & VM_SHARED)) {
-				inc_mm_counter(vma->vm_mm, MM_ANONPAGES);
-				page_add_new_anon_rmap(page, vma, vmf->address, false);
-				lru_cache_add_inactive_or_unevictable(page, vma);
-			} else {
-				inc_mm_counter(vma->vm_mm, mm_counter_file(page));
-				page_add_file_rmap(page, false);
-			}
-			set_pte_at(vma->vm_mm, vmf->address, vmf->pte, entry);
-#endif
+
 			copy_user_highpage(page, vmf->page, vmf->address, vmf->vma);
 			set_smm_page_myself(page);
 			unlock_page(vmf->page);
